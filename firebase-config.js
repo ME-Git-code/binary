7d5a19b314a7e53be1618d27e6a98465cfc6610e
Ozodbek <raximjonozod1727@gmail.com>
2026-08-12 01:18:17 +0500

    gitignore tuzatildi

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// firebase-config.js
+// DIQQAT: Bu fayl haqiqiy Firebase kalitlaringizni saqlaydi.
+// U .gitignore orqali Git repozitoriysiga TUSHMAYDI.
+// Yangi qurilma/kompyuterda ishlash uchun firebase-config.example.js
+// faylidan nusxa oling va shu nom bilan saqlang: firebase-config.js
+
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAhzXpd2nNgeHcEyRTv6aKpUub_XfeyLqc",
+  authDomain: "binary-6.firebaseapp.com",
+  projectId: "binary-6",
+  storageBucket: "binary-6.firebasestorage.app",
+  messagingSenderId: "1008814229589",
+  appId: "1:1008814229589:web:18ea52d2e936d8a17e2cdc",
+  measurementId: "G-QG0E54HM35",
+};
